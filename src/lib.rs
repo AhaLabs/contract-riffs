d73aa0ff710dfad03b3e4b86312885aa967ef0dc
Willem Wyndham <willem@ahalabs.dev>
2022-11-01 18:04:34 -0400

    feat!: make refund functions return result of closure

--- src/lib.rs
+++ src/lib.rs
@@ -20,21 +20,21 @@ pub mod prelude {
 }
 
 /// Mesaure cost of storage for a function
-pub fn measure_storage_cost<F: FnOnce()>(f: F) -> u128 {
+pub fn measure_storage_cost<O, F: FnOnce() -> O>(f: F) -> (O, u128) {
     let bytes_used_before = env::storage_usage();
-    f();
+    let res = f();
     let bytes_used = env::storage_usage() - bytes_used_before;
-    env::storage_byte_cost() * bytes_used as u128
+    (res, env::storage_byte_cost() * bytes_used as u128)
 }
 
-pub fn left_over_balance<F: FnOnce()>(f: F) -> u128 {
-    let cost = measure_storage_cost(f);
+pub fn left_over_balance<O, F: FnOnce() -> O>(f: F) -> (O, u128) {
+    let (res, cost) = measure_storage_cost(f);
     let attached_deposit = near_sdk::env::attached_deposit();
     require!(
         attached_deposit >= cost,
         "Not enough attached deposit to cover storage"
     );
-    attached_deposit - cost
+    (res, attached_deposit - cost)
 }
 
 /// Excute function f, then transfer funds left over after charging for storage stake.
@@ -45,12 +45,13 @@ pub fn left_over_balance<F: FnOnce()>(f: F) -> u128 {
 /// })
 /// }
 /// ```
-pub fn refund_storage_cost<F: FnOnce()>(f: F) {
-    let amount_to_refund = left_over_balance(f);
+pub fn refund_storage_cost<O, F: FnOnce() -> O>(f: F) -> O {
+    let (res, amount_to_refund) = left_over_balance(f);
     if 0 < amount_to_refund {
         let promise_index = reg::promise_batch_create_for_predecessor();
-        env::promise_batch_action_transfer(promise_index, amount_to_refund)
+        env::promise_batch_action_transfer(promise_index, amount_to_refund);
     }
+    res
 }
 
 pub trait IntoKey {
